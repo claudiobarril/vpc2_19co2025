8398f4b07f20bcba39d99dbb80af816d007bbb1c
Claudio Barril <claudiobarril@gmail.com>
2025-07-28 23:22:36 -0300

    baseline ready

---
+++ src/maps.py
@@ -0,0 +1,41 @@
+# Species mapping
+species_es_map = {
+    'Strawberry': 'Fresa',
+    'Grape': 'Uva',
+    'Potato': 'Papa',
+    'Blueberry': 'Arándano',
+    'Corn_(maize)': 'Maíz',
+    'Tomato': 'Tomate',
+    'Peach': 'Durazno',
+    'Pepper,_bell': 'Pimiento',
+    'Orange': 'Naranja',
+    'Cherry_(including_sour)': 'Cereza',
+    'Apple': 'Manzana',
+    'Raspberry': 'Frambuesa',
+    'Squash': 'Calabaza',
+    'Soybean': 'Soja'
+}
+
+# Disease mapping
+disease_es_map = {
+    'Black_rot': 'Podredumbre negra',
+    'Early_blight': 'Tizón temprano',
+    'Target_Spot': 'Mancha diana',
+    'Late_blight': 'Tizón tardío',
+    'Tomato_mosaic_virus': 'Virus del mosaico',
+    'Haunglongbing_(Citrus_greening)': 'Huanglongbing (HLB) / enverdecimiento de los cítricos',
+    'Leaf_Mold': 'Moho de la hoja',
+    'Leaf_blight_(Isariopsis_Leaf_Spot)': 'Tizón de la hoja (mancha foliar por Isariopsis)',
+    'Powdery_mildew': 'Oídio (cenicilla o polvillo blanco)',
+    'Cedar_apple_rust': 'Roya del manzano y cedro',
+    'Bacterial_spot': 'Mancha bacteriana',
+    'Common_rust_': 'Roya común',
+    'Esca_(Black_Measles)': 'Esca (manchas negras)',
+    'Tomato_Yellow_Leaf_Curl_Virus': 'Virus del rizado amarillo de la hoja',
+    'Apple_scab': 'Sarna',
+    'Northern_Leaf_Blight': 'Tizón foliar del norte',
+    'Spider_mites Two-spotted_spider_mite': 'Ácaros araña de dos manchas',
+    'Septoria_leaf_spot': 'Mancha foliar por septoria',
+    'Cercospora_leaf_spot Gray_leaf_spot': 'Mancha foliar por cercospora / mancha foliar gris',
+    'Leaf_scorch': 'Quemadura de la hoja'
+}
